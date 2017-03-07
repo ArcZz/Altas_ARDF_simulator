8250cba70fd32f100872c082d609a26f856cb7c7
zhang zhi <Zhang48240@gmail.com>
2017-03-07 12:20:16 -0600

    gitupdate

---
+++ new/Assets/Script/UI/MenuCtrl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuCtrl : MonoBehaviour {
+
+	public void LoadScene(string sceneName)
+
+	{
+
+		SceneManager.LoadScene("Main_Scene");
+
+	}
+	}
+}
